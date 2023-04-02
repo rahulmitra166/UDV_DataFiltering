330fe123107220a875d79a89c2a3eaeb66700caf
rahulmitra166 <rahul.dpsnewtown@gmail.com>
2023-04-02 15:25:32 +0200

    updated plotting functions

--- udv_analysis_lib.py
+++ udv_analysis_lib.py
@@ -72,103 +72,19 @@ class UDV:
         interpolated_data = interpolation_func(np.arange(len(data)))
         return interpolated_data
     
-    def plot_raw_UDV(self, time, depth, data, xlimits, levels=300):
-        fig1 = plt.figure(1)
+    def plot_data(self, fig_title, fig_num, time, depth, data, xlimits, levels=300):
+        fig = plt.figure(fig_num)
         plt.clf()
         plt.gcf().set_size_inches([16,4])
 
         plt.contourf(time, depth, data, levels, cmap='viridis')
-        plt.title("Raw Data")
+        plt.title(fig_title)
         plt.xlim(xlimits)
         cb = plt.colorbar()
         cb.set_ticks([data.min(), data.min()+((data.max()-data.min())/2), data.max()])
         plt.ylabel('Depth (mm)')
         plt.xlabel("Time (s)")
         plt.tight_layout()
-
-        return fig1
-    
-    def plot_filtered_UDV(self, time, depth, data, xlimits, levels=300):
-        fig2 = plt.figure(2)
-        plt.clf()
-        plt.gcf().set_size_inches([16,4])
-
-        plt.contourf(time, depth, data, levels, cmap='viridis')
-        plt.title("Filtered Data")
-        plt.xlim(xlimits)
-        cb = plt.colorbar()
-        cb.set_ticks([data.min(), data.min()+((data.max()-data.min())/2), data.max()])
-        plt.ylabel('Depth (mm)')
-        plt.xlabel("Time (s)")
-        plt.tight_layout()
-
-        return fig2
-
-    def plot_lineplot(self, time, depth, raw_data, data, xlimits, depthLine = 150):
-
-        fig3 = plt.figure(3)
-        plt.clf()
-        plt.gcf().set_size_inches([16,4])
-
-        plt.plot(time, raw_data[np.searchsorted(depth, depthLine), :], "-r", label="Raw")
-        plt.plot(time, data[np.searchsorted(depth, depthLine), :], "-b", label="Filt.")
-        plt.xlim(xlimits)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Velocity (mm/s)")
-        plt.legend()
-        plt.tight_layout()
-
-        return fig3
-    
-    def plot_averageVz(self, depth, data, start_id_depth = 30):
-        average_velo = np.nanmean(data, axis=1)
-        fig4 = plt.figure(4)
-        plt.clf()
-        plt.gcf().set_size_inches([16,4])
-
-        plt.plot(depth[start_id_depth:], average_velo[start_id_depth:], "-k", label="Mean vz")
-        plt.xlabel("Average Vz (mm/s)")
-        plt.ylabel("Depth (mm)")
-        plt.tight_layout()
-
-        return fig4
-    
-    def plotUDV(self, time, depth, raw_data, data, xlimits, start_id_depth=50, depthLine=150, levels=300):
-        """
-        Arguments
-        ---------
-        depthLine: int or float
-            Depth at which lineplot is to be plotted
-        xlimits: tuple
-            X limits of plot
-        levels: int
-            Number of colorbar levels
-        """
-        avg_velo = np.nanmean(data, axis=1)
-        
-        fig, ax = plt.subplots(4, 1, figsize=(16, 12), constrained_layout=True)
-
-        im1 = ax[0].contourf(time, depth, raw_data, levels, cmap='viridis')
-        ax[0].set_xlim(xlimits)
-        cb1 = fig.colorbar(im1, ax=ax[0])
-        cb1.set_ticks([raw_data.min(), raw_data.min() + ((raw_data.max() - raw_data.min()) / 2), raw_data.max()])
-        ax[0].set_ylabel('Raw')
-
-        im2 = ax[1].contourf(time, depth, data, levels, cmap ='viridis')
-        ax[1].set_xlim(xlimits)
-        cb2 = fig.colorbar(im2, ax=ax[1])
-        cb2.set_ticks([data.min(), data.min() + ((data.max() - data.min()) / 2), data.max()])
-        ax[1].set_ylabel('Filtered')
-
-        ax[2].plot(time, raw_data[np.searchsorted(depth, depthLine), :], "-r", label="Raw")
-        ax[2].plot(time, data[np.searchsorted(depth, depthLine), :], "-b", label="Filt.")
-        ax[2].set_xlim(xlimits)
-        ax[2].set_xlabel('Time (s)')
-        ax[2].legend()
-
-        ax[3].plot(depth[start_id_depth:], avg_velo[start_id_depth:], label="Mean vz")
-        ax[3].set_xlabel("Depth (mm)")
-        ax[3].legend()
 
         return fig
     
